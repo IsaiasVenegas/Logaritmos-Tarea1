9ac902b48876eb9a0cd02493c4a8126fb12e247b
MagicDog12 <creature.secret@gmail.com>
2023-05-05 21:22:40 -0400

    modify for n > for i

--- tarea1.c
+++ tarea1.c
@@ -8,7 +8,8 @@ int main() {
 
     int *pi, *pi_inversa;
 
-    for (int n = pow(2, 20); n <= pow(2, 30); n *= pow(2, 2)) {
+    for (int i = 0; i <= 7; i++) {
+        int n = pow(2, 20 + i);
         clocks_spent_permutations = 0.0;
         clocks_spent_merge_sort = 0.0;
 
@@ -20,20 +21,22 @@ int main() {
             pi_inversa = permutation(pi, n);
             end = clock();
             clocks_spent_permutations += (double)(end - begin);
-            printf("(%d) The elapsed time for %d permutations - permutations is %f seconds\n", i_test, n, (end - begin) / CLOCKS_PER_SEC);
-
+            printf("(%d) The elapsed time for 2^%d permutations - permutations is %f seconds\n", i_test, 20+i, (double)(end - begin) / CLOCKS_PER_SEC);
+            free(pi_inversa);
             // Second algorithm
             begin = clock();
             pi_inversa = inversa(pi, n);
             end = clock();
             clocks_spent_merge_sort += (double)(end - begin);
-            printf("(%d) The elapsed time for %d permutations - merge sort is %f seconds\n", i_test, n, (end - begin) / CLOCKS_PER_SEC);
+            printf("(%d) The elapsed time for 2^%d permutations - merge sort is %f seconds\n", i_test, 20+i, (double)(end - begin) / CLOCKS_PER_SEC);
+            free(pi);
+            free(pi_inversa);
         }
         avg_time_spent_permutations = clocks_spent_permutations / (CLOCKS_PER_SEC * 10);
         avg_time_spent_merge_sort = clocks_spent_merge_sort / (CLOCKS_PER_SEC * 10);
 
-        printf("The avg for %d permutations - permutations is %f seconds\n", n, avg_time_spent_permutations);
-        printf("The avg for %d permutations - merge sort is %f seconds\n", n, avg_time_spent_merge_sort);
+        printf("The avg for 2^%d permutations - permutations is %f seconds\n", 20+i, avg_time_spent_permutations);
+        printf("The avg for 2^%d permutations - merge sort is %f seconds\n", 20+i, avg_time_spent_merge_sort);
     }
 
     return 0;
