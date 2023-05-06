36bae480a7ca92e569792c216148ca942e0b9700
MagicDog12 <creature.secret@gmail.com>
2023-05-06 16:05:33 -0400

    fix milliseconds and standar deviation

--- tarea1.c
+++ tarea1.c
@@ -9,12 +9,12 @@ typedef struct {
 
 int main() {
     Clocks_spent* times = malloc(10 * sizeof(Clocks_spent));
-    double avg_permutations, avg_merge_sort, sd_permutations, sd_merge_sort;
+    int avg_permutations, avg_merge_sort, sd_permutations, sd_merge_sort;
     clock_t begin, end;
 
     int *pi, *pi_inversa;
 
-    for (int i = 0; i <= 7; i++) {
+    for (int i = 0; i <= 8; i++) {
         int n = pow(2, 20 + i);
         avg_permutations = 0.0;
         avg_merge_sort = 0.0;
@@ -29,15 +29,15 @@ int main() {
             begin = clock();
             pi_inversa = permutation(pi, n);
             end = clock();
-            times[i_test-1].permutations = (double)(end - begin) / CLOCKS_PER_SEC;
-            printf("(%d) The elapsed time for 2^%d permutations - permutations is %f seconds\n", i_test, 20+i, times[i_test-1].permutations);
+            times[i_test-1].permutations = ((double)(end - begin) / CLOCKS_PER_SEC) *1000.0;
+            printf("(%d) The elapsed time for 2^%d permutations - permutations is %d milliseconds\n", i_test, 20+i, (int)times[i_test-1].permutations);
             free(pi_inversa);
             // Second algorithm
             begin = clock();
             pi_inversa = inversa(pi, n);
             end = clock();
-            times[i_test-1].merge_sort = (double)(end - begin) / CLOCKS_PER_SEC;
-            printf("(%d) The elapsed time for 2^%d permutations - merge sort is %f seconds\n", i_test, 20+i, times[i_test-1].merge_sort);
+            times[i_test-1].merge_sort = ((double)(end - begin) / CLOCKS_PER_SEC) * 1000.0;
+            printf("(%d) The elapsed time for 2^%d permutations - merge sort is %d milliseconds\n", i_test, 20+i, (int)times[i_test-1].merge_sort);
             free(pi);
             free(pi_inversa);
         }
@@ -46,18 +46,20 @@ int main() {
             avg_permutations += times[j].permutations;
             avg_merge_sort += times[j].merge_sort;
         }
-        avg_permutations = avg_permutations / 10.0;
-        avg_merge_sort = avg_merge_sort / 10.0;
+        avg_permutations = avg_permutations / 10;
+        avg_merge_sort = avg_merge_sort / 10;
         // Acá se calculan las desviaciones estandar
         for(int j=0; j<10; j++){
             sd_permutations += pow(fabs(times[j].permutations - avg_permutations), 2);
             sd_merge_sort += pow(fabs(times[j].merge_sort - avg_merge_sort), 2);
         }
+        sd_permutations = (int) sqrt(sd_permutations / 10.0);
+        sd_merge_sort = (int) sqrt(sd_merge_sort / 10.0);
 
-        printf("The avg for 2^%d permutations - permutations is %f seconds\n", 20+i, avg_permutations);
-        printf("The sd for 2^%d permutations - permutations is %f seconds\n", 20+i, sd_permutations);
-        printf("The avg for 2^%d permutations - merge sort is %f seconds\n", 20+i, avg_merge_sort);
-        printf("The sd for 2^%d permutations - merge sort is %f seconds\n", 20+i, sd_merge_sort);
+        printf("The avg for 2^%d permutations - permutations is %d milliseconds\n", 20+i, avg_permutations);
+        printf("The sd for 2^%d permutations - permutations is %d milliseconds\n", 20+i, sd_permutations);
+        printf("The avg for 2^%d permutations - merge sort is %d milliseconds\n", 20+i, avg_merge_sort);
+        printf("The sd for 2^%d permutations - merge sort is %d milliseconds\n", 20+i, sd_merge_sort);
     }
 
     return 0;
