f37635363ce0aa0934de49eb595d6bd29f85c479
MagicDog12 <creature.secret@gmail.com>
2023-05-07 14:19:27 -0400

    add merge sort arity functional with arity<16

--- mergeSortArity.c
+++ mergeSortArity.c
@@ -9,14 +9,18 @@ typedef struct {
 } Pair;
 
 // Funcion para obtener el sub arreglo al que pertenece el minimo entre los valores
-int findMin(int *v, int arity) {
+int findMin(Pair **p, int arity) {
     int min = INT_MAX;
     int minIndex = -1;
 
     for(int i=0; i<arity; i++) {
-        if(v[i] < min) {
-            min = v[i];
-            minIndex = i;
+        Pair* p_i = p[i];
+        if(p_i != NULL){
+            int v = (*p[i]).value;
+            if(v < min) {
+                min = v;
+                minIndex = i;
+            }
         }
     }
     
@@ -25,7 +29,7 @@ int findMin(int *v, int arity) {
 
 // Función para fusionar dos subarreglos ya ordenados
 void merge(Pair* arr, int l, int* m, int r, int arity) {
-    int *n = malloc(arity * sizeof(int));
+    int n[arity];
     n[0] = m[0] - l + 1; 
     for(int i=1; i<arity-1; i++){
         n[i] = m[i]-m[i-1];
@@ -33,64 +37,67 @@ void merge(Pair* arr, int l, int* m, int r, int arity) {
     n[arity-1]= r - m[arity-2];
     // Recuperamos cada arreglo
     Pair **S = malloc(arity * sizeof(Pair*));
+    if(S == NULL) {
+        printf("Error: No se pudo asignar memoria para s.\n");
+        exit(1);
+    }
     S[0] = malloc(n[0] * sizeof(Pair));
+    if(S[0] == NULL) {
+        printf("Error: No se pudo asignar memoria para S[0].\n");
+        exit(1);
+    }
     for(int j=0; j<n[0]; j++){
             S[0][j] = arr[l + j];
         }
     for(int i=1; i<arity; i++) {
         S[i] = malloc(n[i] * sizeof(Pair));
+        if(S[i] == NULL) {
+            printf("Error: No se pudo asignar memoria para S[%d].\n", i);
+            exit(1);
+        }
         for(int j=0; j<n[i]; j++){
             S[i][j] = arr[m[i-1] + 1 + j];
         }
     }
     // Ordenamos por comparacion de los cuatro subarreglos
-    int* v = malloc(arity * sizeof(int));
-    int* p = malloc(arity * sizeof(int));
+    Pair** p = malloc(arity * sizeof(Pair*));
+    if(p == NULL) {
+        printf("Error: No se pudo asignar memoria para p.\n");
+        exit(1);
+    }
     for(int i=0; i<arity; i++) {
-        v[i] = INT_MAX;
-        p[i] = 0;
+        if(n[i]>0){
+            p[i] = S[i];
+        } else {
+            p[i] = NULL;
+        } 
     }
     int k = l;
-
-    // Si el puntero no ha llegado al final
-    for(int i=0; i<arity; i++) {
-        if(p[i]<n[i]) {
-            v[i] = S[i][p[i]].value;
-        }
-    }
-    int minBelongsTo = findMin(v, arity);
+    int minBelongsTo = findMin(p, arity);
 
     while(minBelongsTo != -1){
-        for(int i=0; i<arity; i++) {
-            if(minBelongsTo == i) {
-                arr[k++] = S[i][p[i]++];
-            }
+        arr[k++] = *p[minBelongsTo];
+        // Si p apunta a una direccion mayor a la direccion que ocupa S
+        if(p[minBelongsTo] + 1 >= S[minBelongsTo] + n[minBelongsTo]){
+            p[minBelongsTo] = NULL;
+        } else {
+            p[minBelongsTo]++;
         }
-        // Actualizamos los valores 
-        for(int i=0; i<arity; i++) {
-            if(p[i]<n[i]) {
-                v[i] = S[i][p[i]].value;
-            } else {
-                v[i] = INT_MAX;
-            }
-        }
-        minBelongsTo = findMin(v, arity);
+        minBelongsTo = findMin(p, arity);
     }
+    free(p);
     for(int i=0; i<arity; i++){
         free(S[i]);
     }
     free(S);
-    free(n);
-    free(v);
-    free(p);
 }
 
 // Merge Sort recursivo
 void mergeSort(Pair* arr, int l, int r, int arity) {
     if (l < r) {
-        int *m = malloc((arity-1) * sizeof(int));
+        int m[(arity-1)];
         for(int i=0; i<arity-1; i++){
-            m[i]= l + (i+1) * (r-l) /4;
+            m[i]= l + (i+1) * (r-l) /arity;
         }
         mergeSort(arr, l, m[0], arity);
         for(int i=1; i<arity-1; i++){
@@ -98,7 +105,6 @@ void mergeSort(Pair* arr, int l, int r, int arity) {
         }
         mergeSort(arr, m[arity-2]+1, r, arity);
         merge(arr, l, m, r, arity);
-        free(m);
     }
 }
 
@@ -106,6 +112,10 @@ void mergeSort(Pair* arr, int l, int r, int arity) {
 int* inversa(int* pi, int n, int arity) {
     // Creamos el arreglo de pares valor-posicion
     Pair *pairs = malloc(n * sizeof(Pair));
+    if(pairs == NULL) {
+        printf("Error: No se pudo asignar memoria para pairs.\n");
+        exit(1);
+    }
     for (int i = 0; i < n; i++) {
         pairs[i].value = pi[i];
         pairs[i].pos = i + 1;
@@ -116,6 +126,10 @@ int* inversa(int* pi, int n, int arity) {
 
     // Proyectamos solo la segunda componente en un nuevo arreglo
     int *proyectado = malloc(n * sizeof(int));
+    if(proyectado == NULL) {
+        printf("Error: No se pudo asignar memoria para proyectado.\n");
+        exit(1);
+    }
     for (int i = 0; i < n; i++) {
         proyectado[i] = pairs[i].pos;
     }
